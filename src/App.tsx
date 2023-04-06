221945403bef37d44c00873af77694244dc8b778
Mariano Moreno Alvariza <mmorenoalvariza@gmail.com>
2023-04-06 19:34:13 -0300

    Search API + Questions

--- src/App.tsx
+++ src/App.tsx
@@ -2,24 +2,27 @@ import { useState } from 'react'
 import './App.css'
 import Autocomplete from './components/Autocomplete'
 import { useQuery } from 'react-query';
-type Character = { id: number, name: string, status: 'Alive' | 'Dead' | 'unknown' };
-const fetchCharacters = async (): Promise<{ results: Array<Character> }> => {
-  const response = await fetch(`https://rickandmortyapi.com/api/character`);
-  return response.json();
-}
+import { Character, fetchCharacters } from './api/api';
+
+
 function App() {
   const [value, setValue] = useState('');
-  //const { isLoading, error, data } = useQuery('characters', () => fetch(`https://rickandmortyapi.com/api/character/?name=${value}`).then(res => res.json()), { enabled: true });
-  const { isLoading, error, data } = useQuery('characters', fetchCharacters);
+ const { isLoading, error, data } = useQuery({queryKey: ['characters', value], queryFn: () => fetchCharacters(value)});
 
-  const options = data?.results?.map((character) => ({ id: character.id, label: character.name, character })) || [];
-  const filteredOptions = options.filter(option => value && option.label.toLowerCase().includes(value.toLowerCase()));
-  const [selectedCharacter, setSelectedCharacter] = useState<typeof filteredOptions[number] | undefined>();
+  const options = data?.results || [];
+  const filteredOptions = options.filter(option => value && option.name.toLowerCase().includes(value.toLowerCase()));
+  const [selectedCharacter, setSelectedCharacter] = useState<Character | undefined>();
   return (<section className="block top-0">
     <div>
-      <Autocomplete placeholder={'Search for Rick and Morty characters'} value={value} onChange={setValue} options={filteredOptions} onSelect={(o) => setSelectedCharacter(o)} />
+      <Autocomplete
+       placeholder={'Search for Rick and Morty characters'}
+       value={value}
+       getLabel={(character) => character.name}
+       onChange={setValue}
+       options={filteredOptions}
+       onSelect={(o) => setSelectedCharacter(o)} />
     </div>
-    {selectedCharacter && <div className='my-40'>Selected character: {selectedCharacter.character.name} and he is {selectedCharacter.character.status}</div>}
+    {selectedCharacter && <div className='my-40'>Selected character: {selectedCharacter.name} and he is {selectedCharacter.status}</div>}
   </section>
   )
 }
--- src/components/Autocomplete.tsx
+++ src/components/Autocomplete.tsx
@@ -1,23 +1,24 @@
 import { FunctionComponent } from "react";
-type Option = { id: string | number; label: string };
+type Option = { id: string | number };
 type Props<T> = {
     placeholder?: string;
     value: string;
+    getLabel: (option: T) => string;
     onChange: (newValue: string) => void;
     options: Array<T>;
     onSelect: (selectedOption: T) => void;
 }
 
 const Autocomplete = <T extends Option,>(props: Props<T>) => {
-    const { placeholder = '', value, onChange, options = [], onSelect } = props;
+    const { placeholder = '', value, getLabel, onChange, options = [], onSelect } = props;
 
     return <div className="flex flex-col -50"><input type='text' className='block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6 border' placeholder={placeholder} value={value} onChange={(e) => onChange(e.target.value)} />
-        <Options options={options} value={value} onSelect={onSelect} /></div>
+        <Options options={options} value={value} onSelect={onSelect} getLabel={getLabel}/></div>
 }
 
-const Options = <T extends Option,>({ options, value, onSelect }: Pick<Props<T>, 'options' | 'value' | 'onSelect'>) => {
+const Options = <T extends Option,>({ options, value, onSelect, getLabel }: Pick<Props<T>, 'options' | 'value' | 'onSelect' | 'getLabel'>) => {
     return <ul className="border flex flex-col justify-start">
-        {options.map(option => <li key={option.id} onClick={() => onSelect(option)}><HighlightLabel label={option.label} value={value} /></li>)}
+        {options.map(option => <li key={option.id} onClick={() => onSelect(option)}><HighlightLabel label={getLabel(option)} value={value} /></li>)}
     </ul>
 }
 
